6cf1b55b656c422148e80dcea92d7e65d18491df
Ivan Lomakov <ivanlomakov@nixsolutions.com>
2017-02-09 15:51:10 +0200

    Launch settings are changed. Application can be launched by parameters from command line

--- Auth/src/Auth.API/Program.cs
+++ Auth/src/Auth.API/Program.cs
@@ -1,16 +1,25 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace Auth.API
 {
     public class Program
     {
-        public static void Main()
+        public static void Main(string[] args)
         {
+            Console.Title = "Auth server (Identity4)";
+
+            var configuration = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseConfiguration(configuration)
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
