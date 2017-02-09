43e0cf9837d35034a4b1a876959507ea967ab2f6
Ivan Lomakov <ivanlomakov@nixsolutions.com>
2017-02-09 13:43:23 +0200

    Base functionality for OAuth 2.0 is added

---
+++ Auth/src/Auth.API/Initialization/FakeDataConfig.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace Auth.Initialization
+{
+    public class FakeDataConfig
+    {
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "UI",
+                    ClientName = "UI Client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "api" }
+                }
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api", "Auth API")
+            };
+        }
+    }
+}
